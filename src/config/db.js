68753733bdf3f7865e10f8f9883b8fee0c323acb
Antony Rithick <antonyrithick78@gmail.com>
2025-06-20 17:44:43 +0530

    register api

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_SERVER, {
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
- dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    }
-  }
+  dialect: process.env.DB_DIALECT,
+  port : process.env.DB_PORT,
+  logging: false,
 });
 
 module.exports = sequelize;
